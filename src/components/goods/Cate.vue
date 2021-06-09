552e8a86b96f94abb96e7ca76659a2ac961bb9ec
JoeyChen <452250588@qq.com>
2021-06-09 20:08:56 +0800

    修改商品分类完成

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -11,29 +11,56 @@
         <el-card>
             <el-row>
                 <el-col>
-                    <el-button type="primary">添加分类</el-button>
+                    <el-button type="primary" @click="showAddCateDialog"
+                        >添加分类</el-button
+                    >
                 </el-col>
             </el-row>
-            <!-- 分类列表区  -->            
+            <!-- 分类列表区  -->
             <el-tree
                 :data="cateList"
                 :props="defaultProps"
                 node-key="id"
-                @node-click="handleNodeClick">
+                @node-click="handleNodeClick"
+            >
                 <template #default="{ node, data }">
-                    <span class="custom-tree-node" style="hight: 30px; border-top: 1px solid rgb(226, 225, 225);">
-                       
-                        <span >{{ node.label }}</span>
-                        <span >
-                            <i class="el-icon-success" v-if="data.cat_deleted===false" style="color:lightgreen;">有效</i>
-                            <i class="el-icon-error" v-else style="color:lightgreen;">无效</i>
+                    <span
+                        class="custom-tree-node"
+                        style="
+                            hight: 30px;
+                            border-top: 1px solid rgb(226, 225, 225);
+                        "
+                    >
+                        <span>{{ node.label }}</span>
+                        <span>
+                            <i
+                                class="el-icon-success"
+                                v-if="data.cat_deleted === false"
+                                style="color: lightgreen"
+                                >有效</i
+                            >
+                            <i
+                                class="el-icon-error"
+                                v-else
+                                style="color: lightgreen"
+                                >无效</i
+                            >
                         </span>
-                        <span >
-                            <el-tag size="mini" v-if="data.cat_level===0">一级</el-tag>
-                            <el-tag size="mini" type="success" v-else-if="data.cat_level===1">二级</el-tag>
-                            <el-tag size="mini" type="warning" v-else>三级</el-tag>
-                        </span>    
-                        <span>                            
+                        <span>
+                            <el-tag size="mini" v-if="data.cat_level === 0"
+                                >一级</el-tag
+                            >
+                            <el-tag
+                                size="mini"
+                                type="success"
+                                v-else-if="data.cat_level === 1"
+                                >二级</el-tag
+                            >
+                            <el-tag size="mini" type="warning" v-else
+                                >三级</el-tag
+                            >
+                        </span>
+                        <span>
                             <el-button
                                 size="mini"
                                 type="primary"
@@ -51,19 +78,56 @@
                         </span>
                     </span>
                 </template>
-            </el-tree>           
+            </el-tree>
             <!-- 分页区 -->
             <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
                 :current-page="queryInfo.pagenum"
-                :page-sizes="[3,5,10,15]"
+                :page-sizes="[3, 5, 10, 15]"
                 :page-size="queryInfo.pagesize"
                 layout="total, sizes, prev, pager, next, jumper"
-                :total="total">
+                :total="total"
+            >
             </el-pagination>
             <el-row></el-row>
         </el-card>
+        <!-- 添加分类对话框 -->
+        <el-dialog
+            title="添加分类"
+            v-model="addCateDialogVisible"
+            width="50%"
+            @close="addCateClose"
+        >
+            <!-- 添加表单 -->
+            <el-form
+                :model="addCateForm"
+                :rules="addCateFormRules"
+                ref="addCateFormRef"
+                label-width="100px"
+            >
+                <el-form-item label="分类名称：" prop="cat_name">
+                    <el-input v-model="addCateForm.cat_name"></el-input>
+                </el-form-item>
+                <el-form-item label="父级分类：">
+                    <el-cascader
+                        v-model="selectedParentKey"
+                        :options="ParentCateList"
+                        :props="cascaderProps"
+                        @change="parentCateChange"
+                        clearable
+                    ></el-cascader>
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">取 消</el-button>
+                    <el-button type="primary" @click="addCate">确 定</el-button>
+                </span>
+            </template>
+        </el-dialog>
+        <!-- 编辑分类对话框 -->
+        
     </div>
 </template>
 
@@ -79,10 +143,30 @@ export default {
                 label: "cat_name",
             },
             cateCurrentData: [],
+            addCateDialogVisible: false,
+            addCateForm: { cat_name: "", cat_pid: 0, cat_level: 0 },
+            addCateFormRules: {
+                cat_name: [
+                    {
+                        required: true,
+                        message: "请输入分类名称",
+                        trigger: "blur",
+                    },
+                ],
+            },
+            ParentCateList: [],
+            selectedParentKey: [],
+            cascaderProps: {
+                expandTrigger: "hover",
+                value: "cat_id",
+                label: "cat_name",
+                children: "children",
+                checkStrictly: true,
+            },
         }
     },
     created() {
-        this.getCateList()
+        this.getCateList(), this.getParentCateList()
     },
     methods: {
         async getCateList() {
@@ -97,18 +181,74 @@ export default {
             // console.log(this.cateList);
         },
         handleSizeChange(newVal) {
-            this.queryInfo.pagesize=newVal
+            this.queryInfo.pagesize = newVal
             this.getCateList()
             // console.log(`每页 ${newVal} 条`);
         },
         handleCurrentChange(newVal) {
-            this.queryInfo.pagenum=newVal
+            this.queryInfo.pagenum = newVal
             this.getCateList()
-        //  console.log(`当前页: ${val}`);
+            //  console.log(`当前页: ${val}`);
         },
-
-
-
+        showAddCateDialog() {
+            this.addCateDialogVisible = true
+        },
+        async getParentCateList() {
+            const { data: res } = await this.$http.get("categories", {
+                params: { type: 2 },
+            })
+            if (res.meta.status !== 200) {
+                return this.$message.error("获取父级分类失败")
+            }
+            // console.log(res);
+            this.ParentCateList = res.data
+        },
+        parentCateChange() {
+            // console.log(value)
+            // this.selectedParentKey = value
+            if (this.selectedParentKey == null) {
+                this.addCateForm.cat_pid = 0
+                this.addCateForm.cat_level = 0
+                console.log(
+                    this.addCateForm.cat_pid,
+                    this.addCateForm.cat_level
+                )
+                return
+            }
+            if (this.selectedParentKey.length > 0) {
+                this.addCateForm.cat_pid = this.selectedParentKey[
+                    this.selectedParentKey.length - 1
+                ]
+                this.addCateForm.cat_level = this.selectedParentKey.length
+                // console.log(this.addCateForm.cat_pid,this.addCateForm.cat_level)
+                return
+            } else {
+                this.addCateForm.cat_pid = 0
+                this.addCateForm.cat_level = 0
+                //  console.log(this.addCateForm.cat_pid,this.addCateForm.cat_level)
+            }
+        },
+        addCateClose() {
+            this.$refs.addCateFormRef.resetFields()
+            this.selectedParentKey = []
+            this.addCateForm = {}
+        },
+        addCate() {
+            this.$refs.addCateFormRef.validate(async (valid) => {
+                if (!valid) return
+                const { data: res } = await this.$http.post(
+                    `categories`,
+                    this.addCateForm
+                )
+                if (res.meta.status !== 201) {
+                    return this.message.error("添加分类失败")
+                }
+                this.$message.success(res.meta.msg) //成功提示
+                this.getCateList() //刷新权限列表
+                this.addCateDialogVisible = false //关 闭弹窗
+            })           
+        },
+        // ----------------------------------
         handleNodeClick(data) {
             // console.log(data);
             this.cateCurrentData = data
@@ -135,15 +275,14 @@ export default {
 </script>
 
 <style scoped>
-.el-tree-node__content {  
-    display: flex;    
+.el-tree-node__content {
+    display: flex;
     -ms-flex-align: center;
     align-items: center;
     height: 40px !important;
-     line-height: 30px !important; 
-   
+    line-height: 30px !important;
 }
-.el-row{
+.el-row {
     margin-bottom: 20px;
 }
 .custom-tree-node {
@@ -152,10 +291,11 @@ export default {
     align-items: center;
     justify-content: space-between;
     font-size: 14px;
-    padding-right: 8px; 
-    padding-left: 10px; 
-     text-align: center;
-    
+    padding-right: 8px;
+    padding-left: 10px;
+    text-align: center;
 }
-
+.el-cascader {
+    width: 100% !important;
+}
 </style>
